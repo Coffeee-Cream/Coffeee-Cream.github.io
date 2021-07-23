e7c91c4c200c98eb1434696dc13df808b7bd434c
Imagineee <imagineeelego@gmail.com>
2021-07-23 16:33:24 +0100

    switching to web worker in progress

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
-window.fs = new LightningFS('fs', { wipe: true })
-window.pfs = window.fs.promises
+import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
 
 var cert
 
-window.onload = function() {
-	window.md = window.markdownit({
+window.onload = async function() {
+	//window.fs = new LightningFS('fs', { wipe: true })
+	//window.pfs = window.fs.promises
+	/*window.md = window.markdownit({
 		html: true,
 		breaks: true,
 		linkify: true,
 		typographer: true
-	})
+	})*/
 	var blogHider = new IntersectionObserver((entries) => {
 		if (entries[0].intersectionRatio <= 0) {document.querySelector('#blog').style.visibility="hidden";return} else {document.querySelector('#blog').style.visibility="visible";}
 	});
@@ -19,87 +19,15 @@ window.onload = function() {
 		if (entries[0].intersectionRatio <= 0) {document.querySelector('#home').style.visibility="hidden";return} else {document.querySelector('#home').style.visibility="visible";}
 	});
 	homeHider.observe(document.querySelector('#home'));
-	getBlogs()
-}
-const dir = '/'
+	
+	const worker = new Worker("js/blogWorker.js", {type: 'module'});
+	const getBlogs = Comlink.wrap(worker);
 
-async function getBlogs() {
-	//get repo
-	await git.clone({
-		fs,
-		http,
-		dir,
-		corsProxy: 'https://cors.isomorphic-git.org',
-		url: 'https://github.com/Coffeee-Cream/blog',
-		ref: 'main',
-		singleBranch: true,
-		depth: 10
-	})
-	//welcome blog
-	//console.log(await pfs.readFile(dir + "README.md", { encoding: 'utf8' }))
-	document.querySelector("#blog > .vertical-center").innerHTML = `<section>${md.render(await pfs.readFile(dir + "README.md", { encoding: 'utf8' }))}<br><div class="scroll">scroll ▼ down</div></section>`
+	worker.onmessage = (e => ViaReceiver.OnMessage(e.data));
 
-	if (navigator.onLine == false) {
-		document.querySelector("#blog-list > .vertical-center").innerHTML = "Your Offline"
-	}
+	await getBlogs()
 
-	//proj list
-	let projsList = await pfs.readFile(dir + "registry/scratch-projects.json", { encoding: 'utf8' })
-	projsList = JSON.parse(projsList)
-	projsList = projsList.data
-	document.querySelector("#scratch-proj > .vertical-center").style.display = "none"
-	for(let i=0;i<projsList.length;i++) {
-		if (i > 5) {
-			let doc = document.createElement("div")
-			doc.innerHTML = `<br>
-			<span style="text-align: center">
-			<a href="https://scratch.mit.edu/users/CoffeeeCream/projects/" target="_blank">More...</a>
-			</span>
-			`
-			document.getElementById("proj-holder").append(doc)
-		} else {
-			let doc = document.createElement("div")
-			//TODO: img loading="lazy"
-			doc.innerHTML = `<img src="https://cdn2.scratch.mit.edu/get_image/project/${projsList[i].id}_144x108.png" width="90%">
-			<br>
-			<span style="text-align: center">
-			<a href="https://scratch.mit.edu/projects/${projsList[i].id}" target="_blank">${projsList[i].name}</a>
-			</span>
-			`
-			document.getElementById("proj-holder").append(doc)
-		}
-	}
-
-	//blog list
-	//blogs/blogs.json
-	let blogList = await pfs.readFile(dir + "blogs/blogs.json", { encoding: 'utf8' })
-	blogList = JSON.parse(blogList)
-
-	document.querySelector("#blog-list").innerHTML = ""
-	for(let i=0;i<blogList.length;i++) {
-		let doc = document.createElement("span")
-		doc.innerHTML = `
-		<a href="/?blog=${blogList[i].name}#read-blog">${blogList[i].name}</a>
-		`
-		document.querySelector("#blog-list").append(doc)
-
-		doc = document.createElement("br")
-		document.querySelector("#blog-list").append(doc)
-	}
-
-	//if read blogs
-	if (window.location.href.indexOf('#read-blog') > -1) {
-		const queryString = window.location.search
-		const urlParams = new URLSearchParams(queryString)
-		const wantedBlog = urlParams.get('blog')
-		for(let i=0;i<blogList.length;i++) {
-			if (wantedBlog == blogList[i].name) {
-				document.querySelector("#read-blog > .vertical-center").style.display = "none"
-				document.getElementById("reader").innerHTML = md.render(await pfs.readFile(dir + "blogs/markup/" + blogList[i].file.location, { encoding: 'utf8' }))
-				document.getElementById("reader").style.display = "block"
-			}
-		}
-	}
+	console.log("finish")
 }
 
 setTimeout(() => {
---
+++ js/blogWorker.js
@@ -0,0 +1,102 @@
+import "https://unpkg.com/comlink/dist/umd/comlink.js";
+import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js';
+import 'https://unpkg.com/isomorphic-git';
+import 'https://unpkg.com/@isomorphic-git/lightning-fs'
+import 'https://cdnjs.cloudflare.com/ajax/libs/markdown-it/12.1.0/markdown-it.min.js'
+//via js
+import "js/viajs/object.js"
+import "js/viajs/property.js"
+import "js/viajs/controller.js"
+
+const getBlogs = async function() {
+	const document = via.document
+
+	const dir = '/'
+
+	var fs = new LightningFS('fs', { wipe: true })
+	var pfs = fs.promises
+	var md = markdownit({
+		html: true,
+		breaks: true,
+		linkify: true,
+		typographer: true
+	})
+	//get repo
+	console.log("giting")
+	await git.clone({
+		fs,
+		http,
+		dir,
+		corsProxy: 'https://cors.isomorphic-git.org',
+		url: 'https://github.com/Coffeee-Cream/blog',
+		ref: 'main',
+		singleBranch: true,
+		depth: 10
+	})
+	//welcome blog
+	//console.log(await pfs.readFile(dir + "README.md", { encoding: 'utf8' }))
+	document.querySelector("#blog > .vertical-center").innerHTML = `<section>${md.render(await pfs.readFile(dir + "README.md", { encoding: 'utf8' }))}<br><div class="scroll">scroll ▼ down</div></section>`
+
+	if (navigator.onLine == false) {
+		document.querySelector("#blog-list > .vertical-center").innerHTML = "Your Offline"
+	}
+
+	//proj list
+	let projsList = await pfs.readFile(dir + "registry/scratch-projects.json", { encoding: 'utf8' })
+	projsList = JSON.parse(projsList)
+	projsList = projsList.data
+	document.querySelector("#scratch-proj > .vertical-center").style.display = "none"
+	for(let i=0;i<projsList.length;i++) {
+		if (i > 5) {
+			let doc = document.createElement("div")
+			doc.innerHTML = `<br>
+			<span style="text-align: center">
+			<a href="https://scratch.mit.edu/users/CoffeeeCream/projects/" target="_blank">More...</a>
+			</span>
+			`
+			document.getElementById("proj-holder").append(doc)
+		} else {
+			let doc = document.createElement("div")
+			doc.innerHTML = `<img src="https://cdn2.scratch.mit.edu/get_image/project/${projsList[i].id}_144x108.png" width="90%"  loading="lazy">
+			<br>
+			<span style="text-align: center">
+			<a href="https://scratch.mit.edu/projects/${projsList[i].id}" target="_blank">${projsList[i].name}</a>
+			</span>
+			`
+			document.getElementById("proj-holder").append(doc)
+		}
+	}
+
+	//blog list
+	//blogs/blogs.json
+	let blogList = await pfs.readFile(dir + "blogs/blogs.json", { encoding: 'utf8' })
+	blogList = JSON.parse(blogList)
+
+	document.querySelector("#blog-list").innerHTML = ""
+	for(let i=0;i<blogList.length;i++) {
+		let doc = document.createElement("span")
+		doc.innerHTML = `
+		<a href="/?blog=${blogList[i].name}#read-blog">${blogList[i].name}</a>
+		`
+		document.querySelector("#blog-list").append(doc)
+
+		doc = document.createElement("br")
+		document.querySelector("#blog-list").append(doc)
+	}
+
+	//if read blogs
+	if (window.location.href.indexOf('#read-blog') > -1) {
+		const queryString = window.location.search
+		const urlParams = new URLSearchParams(queryString)
+		const wantedBlog = urlParams.get('blog')
+		for(let i=0;i<blogList.length;i++) {
+			if (wantedBlog == blogList[i].name) {
+				document.querySelector("#read-blog > .vertical-center").style.display = "none"
+				document.getElementById("reader").innerHTML = md.render(await pfs.readFile(dir + "blogs/markup/" + blogList[i].file.location, { encoding: 'utf8' }))
+				document.getElementById("reader").style.display = "block"
+			}
+		}
+	}
+}
+
+Comlink.expose(getBlogs)
