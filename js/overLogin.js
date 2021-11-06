0479a77b8a3979408e54ba3fe4c5271f54b38444
Imagineee <imagineeelego@gmail.com>
2021-11-06 17:36:26 +0000

    added email btn

--- js/overLogin.js
+++ js/overLogin.js
@@ -1,5 +1,5 @@
 if (localStorage.getItem('login') == 'true') {
-if (new Date().getTime() > parseInt(localStorage.getItem('lastLogin'))+(60*60*24*30)) {
+if (new Date().getTime() > parseInt(localStorage.getItem('lastLogin'))+(1000*60*60*24*30)) {
 localStorage.setItem('login', 'false')
 localStorage.setItem('lastLogin', Date.now())
 }
